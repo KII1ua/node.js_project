653aa8ccc5b54f5c5b1421f68844e4c9cc1b80bd
KII1ua <sung25480@naver.com>
2024-06-19 03:03:33 +0900

    almost_final7

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const homeRoutes = require('./routes/routes'); // connection 객체 전달
 
 app.use('/', homeRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
